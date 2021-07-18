a3503a44a1c87f65f4f20832d7b2fe65376e0537
Muhammad Agung Setya Budi <setyaagung74@gmail.com>
2021-07-18 11:57:21 +0700

    kurang login

--- resources/views/surat-keterangan.blade.php
+++ resources/views/surat-keterangan.blade.php
@@ -6,22 +6,29 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Hasil Pemeriksaan {{ $cv->pasien->nama_pasien}}</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
+    <style type="text/css">
+        @media(max-width: 768px){
+            .logo{
+                text-align: center
+            }
+        }
+    </style>
 </head>
 <body>
     <div class="container">
         <div class="header mt-5">
             <div class="row">
-                <div class="col-md-2">
+                <div class="col-sm-2 logo">
                     <img src="{{ asset('images/logo.jpeg')}}" class="img-fluid" style="width: 120px" alt="">
                 </div>
-                <div class="col-md-8 text-center">
+                <div class="col-sm-8 text-center">
                     <h4 class="font-weight-bold text-success">KLINIK PRATAMA 24 JAM<br>SUMBER MEDIKA MRANGGEN</h4>
                     <p style="font-size: 13px;margin-top: -5px;">
                         Jl. Kangkung Krajan Raya RT. 02/RW. 01, Desa KangKung, Kecamatan Mranggen<br>
                         Kabupaten Demak, Kode Pos 59567 Telp. (024) 76418330, email:kliniksumbermedika12@yahoo.com
                     </p>
                 </div>
-                <div class="col-md-2"></div>
+                <div class="col-sm-2"></div>
             </div>
             <hr style="border: 1px solid black;margin-top: -15px">
         </div>
