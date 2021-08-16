6a0e8811bdfbc44d6689b6b430d9808406f5b0ca
Muhammad Agung Setya Budi <setyaagung74@gmail.com>
2021-08-16 14:39:25 +0700

    vaksin done

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -4,7 +4,10 @@ namespace App\Http\Controllers;
 
 use App\Model\Covid;
 use App\Model\Doctor;
+use App\Model\KeteranganDokter;
+use App\Model\Laboratorium;
 use App\Model\Pasien;
+use App\Model\Vaksin;
 use Illuminate\Http\Request;
 
 class DashboardController extends Controller
@@ -14,6 +17,9 @@ class DashboardController extends Controller
         $dokter = Doctor::count();
         $pasien = Pasien::count();
         $covid = Covid::count();
-        return view('backend.dashboard', \compact('dokter', 'pasien', 'covid'));
+        $lab = Laboratorium::count();
+        $kd = KeteranganDokter::count();
+        $vaksin = Vaksin::count();
+        return view('backend.dashboard', \compact('dokter', 'pasien', 'covid', 'lab', 'kd', 'vaksin'));
     }
 }
--- resources/views/backend/dashboard.blade.php
+++ resources/views/backend/dashboard.blade.php
@@ -47,6 +47,38 @@
                         <a href="{{ route('covid.index')}}" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
                     </div>
                 </div>
+                <div class="col-lg-3 col-6">
+                    <!-- small box -->
+                    <div class="small-box bg-danger">
+                        <div class="inner">
+                            <h3>{{ $lab}}</h3>
+                            <p>Pemeriksaan Laboratorium</p>
+                        </div>
+                        <a href="{{ route('laboratorium.index')}}" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-lg-3 col-6">
+                    <!-- small box -->
+                    <div class="small-box bg-primary">
+                        <div class="inner">
+                            <h3>{{ $vaksin}}</h3>
+                            <p>Vaksin</p>
+                        </div>
+                        <a href="{{ route('vaksin.index')}}" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-6">
+                    <!-- small box -->
+                    <div class="small-box bg-secondary">
+                        <div class="inner">
+                            <h3>{{ $kd}}</h3>
+                            <p>Keterangan Dokter</p>
+                        </div>
+                        <a href="{{ route('keterangan-dokter.index')}}" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+                    </div>
+                </div>
             </div>
         </div>
     </section>
